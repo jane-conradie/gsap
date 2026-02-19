e1e8da3dfd7a82de925d39cb6a66c9b39c394a72
jane <jane@habanerosystems.com>
2026-02-19 17:12:34 +0200

    common easing examples

---
+++ part5 - eases/script.js
@@ -0,0 +1,47 @@
+gsap.to(".bounce", {
+    x: 800,
+    ease: "bounce.in",
+    duration: 5,
+    repeat: -1,
+    yoyo: true
+});
+
+gsap.to(".elastic", {
+    x: 800,
+    ease: "elastic.in",
+    duration: 5,
+    repeat: -1,
+    yoyo: true
+});
+
+gsap.to(".expo", {
+    x: 800,
+    ease: "expo.inOut",
+    duration: 5,
+    repeat: -1,
+    yoyo: true
+});
+
+gsap.to(".power", {
+    x: 800,
+    ease: "power3.inOut",
+    duration: 5,
+    repeat: -1,
+    yoyo: true
+});
+
+gsap.to(".sine", {
+    x: 800,
+    ease: "sine.inOut",
+    duration: 5,
+    repeat: -1,
+    yoyo: true
+});
+
+gsap.to(".steps", {
+    x: 800,
+    ease: "steps(5)",
+    duration: 5,
+    repeat: -1,
+    yoyo: true
+});
